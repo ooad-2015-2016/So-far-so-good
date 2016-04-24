e4dec11ecd4307fad8453e164987b0f9994f872a
HajdarevicEna <ehajdarevi2@etf.unsa.ba>
2016-04-25 00:00:52 +0200

    Dodala klasu Oglasavanje.

---
+++ ProjekatJobRadar/JobRadar/OglasavanjeBaza/Models/Oglasavanje.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobRadar.OglasavanjeBaza.Models
+{
+    class Oglasavanje
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idOglasa { get; set; }
+        //private Poslodavac poslodavac { get; set; }
+        private int brojPregleda { get; set; }
+        private Boolean oglasavanjeKompanije = false;
+        //private Konkurs konkurs { get; set; }
+        private DateTime datumAktiviranja { get; set; }
+        private DateTime datumIsteka { get; set; }
+        private Boolean istekao = false;
+
+        public bool OglasavanjeKompanije
+        {
+            get
+            {
+                return oglasavanjeKompanije;
+            }
+
+            set
+            {
+                oglasavanjeKompanije = value;
+            }
+        }
+
+        public bool Istekao
+        {
+            get
+            {
+                return istekao;
+            }
+
+            set
+            {
+                istekao = value;
+            }
+        }
+
+
+    }
+}
