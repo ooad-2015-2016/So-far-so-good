bcfa45fa90b1b87fcd496c45111a0e84e1f1f357
ebeus <ebeus1@etf.unsa.ba>
2016-04-24 21:21:11 +0200

    Update

--- ProjekatJobRadar/JobRadar/JobRadarBaza/Models/Konkurs.cs
+++ ProjekatJobRadar/JobRadar/JobRadarBaza/Models/Konkurs.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using JobRadar.LokacijaBaza.Models;
 namespace JobRadar.JobRadarBaza.Models
 {
     class Konkurs
@@ -22,5 +22,34 @@ namespace JobRadar.JobRadarBaza.Models
         {
 
         } 
+
+        public int getID() { return idKonkursa; }
+        public string getNaziv() { return NazivKonkursa; }
+        public DateTime getDatumObjave() { return datumObjave; }
+        public DateTime getDatumIsteka() { return datumIsteka; }
+        public Lokacija getLokacija() { return lokacijaPosla; }
+        public bool getJavnoVidljiv() { return javnoVidljiv; }
+        public bool getAktivan() { return aktivan; }
+        public int getBrojPregleda() { return brojPregleda; }
+
+        public void setNaziv(string naziv)
+        {
+            this.NazivKonkursa = naziv;
+        }
+
+        public void setDatumIsteka(DateTime datum)
+        {
+            this.datumIsteka = datum;
+        }
+
+        public void setLokacija(Lokacija lokacija)
+        {
+            this.lokacijaPosla = lokacija;
+        }
+
+        public void setJavnoVidljiv(bool vidljiv)
+        {
+            this.javnoVidljiv = vidljiv;
+        }
     }
 }
---
+++ ProjekatJobRadar/JobRadar/JobRadarBaza/Models/OsobaKojaTraziPosao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobRadar.JobRadarBaza.Models
+{
+    public class OsobaKojaTraziPosao:Korisnik
+    {
+        private string ime { get; set; }
+        private string prezime { get; set; }
+        private DateTime datumRodjenja { get; set; }
+        private DateTime godinaZavrsetkaObrazovanja { get; set; }
+        //file CV dodati
+        private bool zaposlen {
+            get; set;
+        }
+   //     public List<Konkurs> prijavljenNaKonkurse { get; set; }
+
+        public OsobaKojaTraziPosao(string ime, string prezime, DateTime datumRodjenja, DateTime godinaZavrsetkaObrazovanja, bool zaposlen, 
+            string username, string email, string passwd):base(username,email,passwd)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.datumRodjenja = datumRodjenja;
+            this.godinaZavrsetkaObrazovanja = godinaZavrsetkaObrazovanja;
+            this.zaposlen = zaposlen;
+          //  this.prijavljenNaKonkurse = prijavljenNaKonkurs;
+        }
+    }
+}
