125ff6dd1e42082133b0fafd8ea4b55ac5c4041c
ebeus <ebeus1@etf.unsa.ba>
2016-05-22 11:51:30 +0200

    Web

---
+++ ProjekatJobRadar/JobRadarWeb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace JobRadarWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ProjekatJobRadar/JobRadarWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JobRadarWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
