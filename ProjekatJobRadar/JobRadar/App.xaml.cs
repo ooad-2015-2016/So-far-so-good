91cf58192680a436e307adddc26db11349e2816c
Jasmina Bajramović <bajramovicjasmina2@gmail.com>
2016-05-23 18:05:07 +0200

    Prva stranica -> Pocetna.xaml

--- ProjekatJobRadar/JobRadar/App.xaml.cs
+++ ProjekatJobRadar/JobRadar/App.xaml.cs
@@ -79,7 +79,7 @@ namespace JobRadar
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(JobRadar.Pocetna), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
