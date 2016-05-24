1a8e0b0f676a7588b38dc17882d2d25eede725d3
Jasmina Bajramović <bajramovicjasmina2@gmail.com>
2016-05-25 00:23:31 +0200

    Dodan CameraHelper.cs

---
+++ ProjekatJobRadar/JobRadar/JobRadarBaza/Models/CameraHelper.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Streams;
+using Windows.System.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+namespace JobRadar.JobRadarBaza.Models
+{
+    public class CameraHelper : INotifyPropertyChanged
+    {
+        
+        private MediaCapture mediaCapture;
+        public MediaCapture MediaCapture
+        {
+            get { return mediaCapture; }
+            set
+            {
+                mediaCapture = value;
+                OnNotifyPropertyChanged("MediaCapture");
+            }
+        }
+      
+        private bool _isInitialized;
+        private bool _isPreviewing;
+        private bool _isRecording;
+        private bool _externalCamera;
+        private bool _mirroringPreview;
+        private readonly DisplayRequest _displayRequest = new DisplayRequest();
+        public string internalStatus;
+        
+        public InMemoryRandomAccessStream Slika { get; set; }
+        
+        public SoftwareBitmapSource SlikaBitmap { get; set; }
+       
+        public CaptureElement PreviewControl { get; set; }
+        public CameraHelper(CaptureElement previewControl)
+        {
+            PreviewControl = previewControl;
+        }
+        public async Task InitializeCameraAsync()
+        {
+            if (MediaCapture == null)
+            {
+               
+                var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+               
+                DeviceInformation cameraDevice =
+                allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null &&
+                x.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Back);
+                
+                cameraDevice = cameraDevice ?? allVideoDevices.FirstOrDefault();
+               if (cameraDevice == null)
+                {
+                    internalStatus = "No camera device found.";
+                    return;
+                }
+               
+                MediaCapture = new MediaCapture();
+                var mediaInitSettings = new MediaCaptureInitializationSettings
+                {
+                    VideoDeviceId =
+               cameraDevice.Id
+                };
+                try
+                {
+                    
+                    await MediaCapture.InitializeAsync(mediaInitSettings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    internalStatus = ("The app was denied access to the camera");
+                }
+                catch (Exception ex)
+                {
+                    internalStatus = ("Exception when initializing MediaCapture with " + cameraDevice.Id +
+                   ex.ToString());
+                }
+             
+                if (_isInitialized)
+                {
+
+                    if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel ==
+    Windows.Devices.Enumeration.Panel.Unknown)
+                    {
+                        _externalCamera = true;
+                    }
+                    else
+                    {
+                        _externalCamera = false;
+                        _mirroringPreview = (cameraDevice.EnclosureLocation.Panel ==
+                        Windows.Devices.Enumeration.Panel.Front);
+                    }
+                    await StartPreviewAsync();
+                }
+            }
+
+        }
+
+
+        private async Task StartPreviewAsync()
+        {
+            
+            _displayRequest.RequestActive();
+          
+            PreviewControl.Source = MediaCapture;
+          
+            PreviewControl.FlowDirection = _mirroringPreview ? FlowDirection.RightToLeft :
+           FlowDirection.LeftToRight;
+            try
+            {
+               
+                await MediaCapture.StartPreviewAsync();
+                _isPreviewing = true;
+            }
+            catch (Exception ex)
+            {
+                internalStatus = ("Exception when starting the preview:" + ex.ToString());
+            }
+        }
+        public async Task TakePhotoAsync(Action<SoftwareBitmapSource> callback)
+        {
+         
+            Slika = new InMemoryRandomAccessStream();
+            try
+            {
+            
+                await MediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateBmp(),
+               Slika);
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(Slika);
+                SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+                SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(softwareBitmap,
+                BitmapPixelFormat.Bgra8,
+                BitmapAlphaMode.Premultiplied);
+                SlikaBitmap = new SoftwareBitmapSource();
+                await SlikaBitmap.SetBitmapAsync(softwareBitmapBGR8);
+                callback(SlikaBitmap);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception when taking a photo: {0}", ex.ToString());
+            }
+        }
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnNotifyPropertyChanged([CallerMemberName] string memberName = "")
+        {
+  
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+    }
+}
---
+++ ProjekatJobRadar/JobRadar/JobRadarBaza/ViewModels/KorisnikViewModel.cs
@@ -0,0 +1,74 @@
+﻿using JobRadar.JobRadarBaza.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace JobRadar.JobRadarBaza.ViewModels
+{
+    class KorisnikViewModel:INotifyPropertyChanged
+    {
+        
+      //  public Korisnik CreateUposlenik { get; set; }
+        ExterniServis eksterniServis;
+        public CameraHelper Camera { get; set; }
+
+    //    public ICommand DodajUposlenika { get; set; }
+        public ICommand Uslikaj { get; set; }
+     
+        private SoftwareBitmapSource slika;
+        public SoftwareBitmapSource Slika
+        {
+            get { return slika; }
+            set
+            {
+                slika = value;
+                OnNotifyPropertyChanged("Slika");
+            }
+        }
+     
+        CaptureElement previewControl;
+        public KorisnikViewModel(CaptureElement previewControl)
+        {
+            
+            eksterniServis = new ExterniServis();
+           // CreateUposlenik = new Uposlenik();
+           
+            Camera = new CameraHelper(previewControl);
+            Camera.InitializeCameraAsync();
+           // DodajUposlenika = new RelayCommand<object>(dodajUposlenika, (object parametar) => true);
+           // Uslikaj = new RellayCommand<object>(uslikaj, (object parametar) => true);
+        }
+        //komanda koja inicira slikanje
+        public async void uslikaj(object parametar)
+        {
+            await Camera.TakePhotoAsync(SlikanjeGotovo);
+        }
+       /*
+        public void dodajUposlenika(object parametar)
+        {
+            eksterniServis.dodajKorisnika(CreateUposlenik);
+            CreateUposlenik = new Uposlenik();
+        }*/
+      
+        public void SlikanjeGotovo(SoftwareBitmapSource slikica)
+        {
+            Slika = slikica;
+        }
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnNotifyPropertyChanged([CallerMemberName] string memberName = "")
+        {
+            //? je skracena verzija ako nije null
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+    }
+}
+
